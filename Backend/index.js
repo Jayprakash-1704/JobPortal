0671fc750dc12fbd37b873b728ab1776005c6732
Jayprakash <142290589+Jayprakash-1704@users.noreply.github.com>
2026-04-26 23:06:55 +0530

    Update CORS options to include production URL

--- Backend/index.js
+++ Backend/index.js
@@ -22,11 +22,16 @@ app.use(cookieParser());
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://your-frontend-name.onrender.com"
+  ],
   credentials: true,
 };
 
 app.use(cors(corsOptions));
+
+
 
 
 //  api routes
